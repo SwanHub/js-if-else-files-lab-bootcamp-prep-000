074664a997085e1ba7c698924c97427359a44d5f
Jackson Prince <jdprince555@gmail.com>
2019-03-06 01:52:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var name = "Susan"
 var height = "74"
 
-if (name === "Susan"){
-  console.log("correctly named the human")
-  } else {
-    console.log("It's actually Suzan");
-  }
-
 
 // Don't worry about this
 module.exports = { name, height
